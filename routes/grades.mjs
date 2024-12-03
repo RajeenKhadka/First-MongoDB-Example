18e21affbd6e56ada477d2e67ada267651887a40
RajeenKhadka <rajeenkhadka315@gmail.com>
2024-12-03 09:47:38 -0500

    added SHOW route

--- routes/grades.mjs
+++ routes/grades.mjs
@@ -13,4 +13,18 @@ router.get("/", async (req, res) => {
   else res.send(results).status(200);
 });
 
+router.get("/:id", async (req, res) => {
+  let collection = await db.collection("grades");
+  let query;
+  try {
+    query = { _id: new ObjectId(req.params.id) };
+    let result = await collection.findOne(query);
+
+    if (!result) res.send("nof found").status(404);
+    else res.send(result).status(200);
+  } catch (err) {
+    res.send("not an id").status(400);
+  }
+});
+
 export default router;
